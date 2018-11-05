8487b9d2c22b3c2c09a4a378da8e03743928df88
akrisnanda <44166275+akrisnanda@users.noreply.github.com>
2018-11-05 11:23:40 +0700

    new upload
    this script for KreasITindo purpose..

---
+++ unsorted-names-list.php
@@ -0,0 +1,41 @@
+<?php
+
+/* Name  : Andry Krisnanda
+	Email : andry.krisnanda@gmail.com
+*/
+
+
+$name = array(
+	 "Orson Milka Iddins"
+	,"Erna Dorey Battelle"
+	,"Flori Chaunce Franzel"
+	,"Odetta Sue Kaspar"
+	,"Roy Ketti Kopfen"
+	,"Madel Bordie Mapplebeck"
+	,"Selle Bellison"
+	,"Leonerd Adda Mitchell Monaghan"
+	,"Debra Micheli"
+	,"Hailey Avie Annakin"
+);
+
+function genString($arr){
+	foreach($arr as $key=>$row){
+		echo $row."<br/>";
+	}
+}
+
+foreach($name as $key=>$row){
+	$pecah = explode(" ",$row);
+	$new_name[$pecah[count($pecah)-1]] = $row;
+}
+
+ksort($new_name);
+
+echo "<b>Input: </b><br/>";
+genString($name);
+
+echo "<br/>";
+
+echo "<b>Output: </b><br/>";
+genString($new_name);
+?>
